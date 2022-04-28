558da9f42b566744a5b4e76f27da53210458d971
FlutLab.io <>
2022-04-28 16:11:26 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class FormAddTask extends StatelessWidget {
           onPressed: () {
             print('click button');
           },
-          style: ButtonStyle(minimumSize: MaterialStateProperty.all(Size(60, 40))),
+          style: ButtonStyle(minimumSize: MaterialStateProperty.all(Size(100, 70))),
         ),
       ]),
     );
