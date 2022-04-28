4fdeca99cbe01bdf13250ff15855c49e86d2ccf2
FlutLab.io <>
2022-04-28 13:05:21 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const TodoListApp());
+
+class TodoListApp extends StatelessWidget {
+  const TodoListApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        // Application name
+        home: Scaffold(
+      body: FormAddTask(),
+    ));
+  }
+}
+
+class FormAddTask extends StatelessWidget {
+  const FormAddTask({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Criando Transferência'),
+      ),
+      body: Column(children: const <Widget>[
+        TextField(),
+      ]),
+    );
+    ;
+  }
+}
+
+class listTasks extends StatelessWidget {
+  const listTasks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Transferências'),
+      ),
+      body: Column(children: <Widget>[
+        TaskTransferencia(Task("100.0", "1000")),
+        TaskTransferencia(Task("200.0", "2000")),
+        TaskTransferencia(Task("300.0", "3000")),
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TaskTransferencia extends StatelessWidget {
+  final Task _task;
+
+  TaskTransferencia(this._task);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.task),
+        title: Text(_task.taskTitle),
+        subtitle: Text(_task.taskSubTitle),
+      ),
+    );
+  }
+}
+
+class Task {
+  final String taskTitle;
+  final String taskSubTitle;
+
+  Task(this.taskTitle, this.taskSubTitle);
+}
